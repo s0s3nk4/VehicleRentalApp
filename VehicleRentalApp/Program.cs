64088f640903f6c4c0a529b76ce439dbb388b9b7
s0s3nk4 <msosna2929@gmail.com>
2025-05-16 11:56:26 +0200

    zmiana na inMemoryDB

--- VehicleRentalApp/Program.cs
+++ VehicleRentalApp/Program.cs
@@ -12,7 +12,7 @@ namespace VehicleRentalApp
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseInMemoryDatabase("TestDatabase"));
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<AppDbContext>();
