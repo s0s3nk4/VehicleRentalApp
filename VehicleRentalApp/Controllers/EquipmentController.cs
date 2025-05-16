6f781bf550e50b7dcf4b300be6274eada5c5e1fe
s0s3nk4 <msosna2929@gmail.com>
2025-05-16 12:12:18 +0200

    wstrzykiwanie do wzorca

---
+++ VehicleRentalApp/Controllers/EquipmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleRentalApp.Repositories.Interfaces;
+
+namespace VehicleRentalApp.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        private readonly IEquipmentRepository _equipmentRepository;
+        public EquipmentController(IEquipmentRepository equipmentRepository)
+        {
+            _equipmentRepository = equipmentRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var equipments = await _equipmentRepository.GetAllAsync();
+            return View(equipments);
+        }
+    }
+}
